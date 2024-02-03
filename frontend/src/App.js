4f29a98c80cc990bf328c195bf44cc3f3cb4fddf
Mouad Tiahi <tiahimouad22@gmail.com>
2024-02-03 00:56:48 -0500

    Made UI look better via CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,21 @@
-//import { parse, evaluate } from '@cortex-js/compute-engine';
 import './App.css';
 import "//unpkg.com/mathlive"; // math live editable math field
-import {useState } from "react";
+import { useState } from "react";
 
-const View = () =>{
-  const initialValue = "f(x) = \\frac{2x}{3}";
-  const [value, setValue] = useState(initialValue);
+const View = () => {
+  const [value, setValue] = useState("f(x) = \\frac{2x}{3}");
 
-
-  // parse the value 
-  //console.log(expr);
-// parses the expression into ["Element", ["Subtract", ["Power", 2, 11] , 1], "PrimeNumber"]
-
-//console.log(evaluate(expr));
-// evaluates the expression and verifies for "False"
   return (
-    
-    <div className="App">
-
+    <div className="App" style={{ backgroundColor: '#ADD8E6' }}>
+      <h1>Math Expression Editor</h1> {"Your Expression: "}
       <math-field
-        onInput = {evt => setValue(evt.target.value)}
-        >{value}</math-field>
-        <p>Value: {value}</p>
+        onInput={evt => setValue(evt.target.value)}
+      >
+        {value}
+      </math-field>
+      <p>Value: {value}</p>
     </div>
   );
-  }
+}
+
 export default View;
