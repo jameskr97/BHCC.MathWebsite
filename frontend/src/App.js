969b463ca19692dbaa3d896e4c992c5d9c567f1b
James <james@jameskr.dev>
2024-02-03 07:05:20 +0000

    Added parsed value, changed UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,32 @@
+import {ComputeEngine} from '@cortex-js/compute-engine';
 import './App.css';
 import "//unpkg.com/mathlive"; // math live editable math field
-import { useState } from "react";
+import {useState} from "react";
 
 const View = () => {
-  const [value, setValue] = useState("f(x) = \\frac{2x}{3}");
+    const ce = new ComputeEngine();
+    const [value, setValue] = useState("f(x) = \\frac{2}{3}");
+    const [parsed, setParsed] = useState(JSON.stringify(ce.parse(value).toJSON()));
 
-  return (
-    <div className="App" style={{ backgroundColor: '#ADD8E6' }}>
-      <h1>Math Expression Editor</h1> {"Your Expression: "}
-      <math-field
-        onInput={evt => setValue(evt.target.value)}
-      >
-        {value}
-      </math-field>
-      <p>Value: {value}</p>
-    </div>
-  );
+
+    return (
+        <div className="App" style={{backgroundColor: '#ADD8E6'}}>
+            <h1>Math Expression Editor</h1>
+
+            <div id="math-field-container">
+                <hr/>
+                <math-field onInput={evt => {
+                    setValue(evt.target.value);
+                    setParsed(JSON.stringify(ce.parse(evt.target.value, {canonical: false}).toJSON()))
+                }}>
+                    {value}
+                </math-field>
+                <hr/>
+                <p>Value: {value}</p>
+                <p>Parsed: {parsed}</p>
+            </div>
+        </div>
+    );
 }
 
 export default View;
